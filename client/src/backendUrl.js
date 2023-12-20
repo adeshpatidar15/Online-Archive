4dceca4ffd452c593c57f4b0acc80f4b100f3555
adeshpatidar15 <adeshpatidar150@gmail.com>
2023-12-20 16:46:56 +0530

    second commit

--- client/src/backendUrl.js
+++ client/src/backendUrl.js
@@ -1,3 +1,4 @@
-const backendUrl = 'http://localhost:3005';
+// const backendUrl = 'http://localhost:3005';
+const backendUrl = 'https://mernbackend-7c73.onrender.com';
 
 export default backendUrl;
